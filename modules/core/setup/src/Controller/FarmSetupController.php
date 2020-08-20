f50c978f24944ff703c1d0a0988c9bccc607d647
paul121 <paul.weidner@gmail.com>
2020-08-20 14:01:50 -0700

    Add setup page with a simple form to install modules.

---
+++ modules/core/setup/src/Controller/FarmSetupController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Drupal\farm_setup\Controller;
+
+use Drupal\Core\Controller\ControllerBase;
+use Drupal\Core\Link;
+use Drupal\Core\Url;
+
+/**
+ * Class FarmSetupController.
+ */
+class FarmSetupController extends ControllerBase {
+
+  /**
+   * Setup page.
+   *
+   * @return array
+   *   Markup.
+   */
+  public function setupPage() {
+    $setup_wizard_url = Url::fromRoute('farm_setup.setup_wizard');
+    $setup_wizard_link = Link::fromTextAndUrl('Go to the wizard', $setup_wizard_url);
+    return [
+      '#type' => 'markup',
+      '#markup' => '<p>' . $this->t('The setup wizard will guide you through the process of configuring your farmOS.') . '</p><p>' . $setup_wizard_link->toString() . '</p>',
+    ];
+  }
+
+}
