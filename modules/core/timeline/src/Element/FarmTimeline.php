78322235d5973791f68bf97fdd54d391e3475b34
Paul Weidner <paul.weidner@gmail.com>
2024-07-25 17:31:01 -0700

    Add farm_timeline render element

---
+++ modules/core/timeline/src/Element/FarmTimeline.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Drupal\farm_timeline\Element;
+
+use Drupal\Component\Utility\Html;
+use Drupal\Core\Render\Element\RenderElement;
+
+/**
+ * Provides a farm timeline render element.
+ *
+ * @RenderElement("farm_timeline")
+ */
+class FarmTimeline extends RenderElement {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getInfo() {
+    return [
+      '#theme' => 'farm_timeline',
+      '#pre_render' => [
+        [get_class($this), 'preRenderTimeline'],
+      ],
+    ];
+  }
+
+  /**
+   * Pre-render callback for the timeline render array.
+   *
+   * @param array $element
+   *   A renderable array for the timeline.
+   *
+   * @return array
+   *   The final render array for the timeline.
+   */
+  public static function preRenderTimeline(array $element): array {
+
+    // Set a timeline ID.
+    if (empty($element['#attributes']['id'])) {
+      $element['#attributes']['id'] = Html::getUniqueId('timeline');
+    }
+
+    // Add the farm-timeline class.
+    $element['#attributes']['class'][] = 'farm-timeline';
+
+    // Attach the farmOS-timeline library.
+    $element['#attached']['library'][] = 'farm_timeline/farmOS-timeline';
+
+    return $element;
+  }
+
+}
