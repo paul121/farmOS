535e2f16938d1890226671e9a1e7e5378dd2d532
Paul Weidner <paul.weidner@gmail.com>
2025-01-16 09:54:23 -0800

    Use PlanInterface type hint

--- modules/core/plan/src/PlanStorage.php
+++ modules/core/plan/src/PlanStorage.php
@@ -80,7 +80,7 @@ class PlanStorage extends SqlContentEntityStorage {
    * {@inheritdoc}
    */
   protected function doPreSave(EntityInterface $entity) {
-    /** @var \Drupal\Core\Entity\ContentEntityInterface $entity */
+    /** @var \Drupal\plan\Entity\PlanInterface $entity */
     $id = parent::doPreSave($entity);
 
     // If there is no original entity, bail.
