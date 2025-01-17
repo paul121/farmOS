d951abd9164a46c4c4acfa4b584f2a9b7d1d6fc1
Paul Weidner <paul.weidner@gmail.com>
2025-01-17 14:49:33 -0800

    Use entity field methods for passRaw property
    Fix access to an undefined property Drupal\Core\Session\AccountInterface::$passRaw.

--- modules/core/login/tests/src/Functional/UserLoginTest.php
+++ modules/core/login/tests/src/Functional/UserLoginTest.php
@@ -4,11 +4,11 @@ declare(strict_types=1);
 
 namespace Drupal\Tests\farm_login\Functional;
 
-use Drupal\Core\Session\AccountInterface;
 use Drupal\Core\StringTranslation\StringTranslationTrait;
 use Drupal\Core\Url;
 use Drupal\Tests\farm_test\Functional\FarmBrowserTestBase;
 use Drupal\user\Entity\User;
+use Drupal\user\UserInterface;
 
 /**
  * Test using an email in the UserLoginForm.
@@ -93,7 +93,8 @@ class UserLoginTest extends FarmBrowserTestBase {
 
     $user1 = $this->drupalCreateUser([]);
     $incorrect_user1 = clone $user1;
-    $incorrect_user1->passRaw .= 'incorrect';
+    $bad_pass = $incorrect_user1->get('passRaw')->value . 'incorrect';
+    $incorrect_user1->set('passRaw', $bad_pass);
 
     $user2 = $this->drupalCreateUser([]);
 
@@ -177,29 +178,29 @@ class UserLoginTest extends FarmBrowserTestBase {
   /**
    * A helper function to login using an email.
    *
-   * @param \Drupal\Core\Session\AccountInterface $account
+   * @param \Drupal\user\UserInterface $user
    *   User object representing the user to log in.
    *
    * @see drupalLogin()
    * @see drupalCreateUser()
    */
-  protected function drupalLoginUsingEmail(AccountInterface $account) {
+  protected function drupalLoginUsingEmail(UserInterface $user) {
     if ($this->loggedInUser) {
       $this->drupalLogout();
     }
 
     $this->drupalGet(Url::fromRoute('user.login'));
     $this->submitForm([
-      'name' => $account->getEmail(),
-      'pass' => $account->passRaw,
+      'name' => $user->getEmail(),
+      'pass' => $user->get('passRaw')->value,
     ], 'Log in');
 
     // @see ::drupalUserIsLoggedIn()
-    $account->sessionId = $this->getSession()->getCookie(\Drupal::service('session_configuration')->getOptions(\Drupal::request())['name']);
-    $this->assertTrue($this->drupalUserIsLoggedIn($account), 'User ' . $account->getAccountName() . ' successfully logged in.');
+    $user->sessionId = $this->getSession()->getCookie(\Drupal::service('session_configuration')->getOptions(\Drupal::request())['name']);
+    $this->assertTrue($this->drupalUserIsLoggedIn($user), 'User ' . $user->getAccountName() . ' successfully logged in.');
 
-    $this->loggedInUser = $account;
-    $this->container->get('current_user')->setAccount($account);
+    $this->loggedInUser = $user;
+    $this->container->get('current_user')->setAccount($user);
   }
 
 }
