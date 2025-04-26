fb59dd2a4cba9759335a36cae9c991e082946a13
Paul Weidner <paul.weidner@gmail.com>
2025-04-25 20:49:41 -0700

    Add attributes for quick form plugins

--- modules/core/quick/tests/modules/farm_quick_test/src/Plugin/QuickForm/RequiresEntityTest.php
+++ modules/core/quick/tests/modules/farm_quick_test/src/Plugin/QuickForm/RequiresEntityTest.php
@@ -4,20 +4,22 @@ declare(strict_types=1);
 
 namespace Drupal\farm_quick_test\Plugin\QuickForm;
 
+use Drupal\Core\StringTranslation\TranslatableMarkup;
+use Drupal\farm_quick\Attribute\QuickForm;
+
 /**
  * Test quick form that requires a configuration entity.
- *
- * @QuickForm(
- *   id = "requires_entity_test",
- *   label = @Translation("Test requiresEntity quick form"),
- *   description = @Translation("Test requiresEntity quick form description."),
- *   helpText = @Translation("Test requiresEntity quick form help text."),
- *   permissions = {
- *     "create test log",
- *   },
- *   requiresEntity = True
- * )
  */
+#[QuickForm(
+  id: 'requires_entity_test',
+  label: new TranslatableMarkup('Test requiresEntity quick form'),
+  description: new TranslatableMarkup('Test requiresEntity quick form description.'),
+  helpText: new TranslatableMarkup('Test requiresEntity quick form help text.'),
+  permissions: [
+    'create test log',
+  ],
+  requiresEntity: TRUE,
+)]
 class RequiresEntityTest extends Test {
 
 }
