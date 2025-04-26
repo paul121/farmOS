0a974489a4621d7fb0422195ed8d4ce0e272f0f7
Paul Weidner <paul.weidner@gmail.com>
2025-04-26 09:00:58 -0700

    Remove processDefinition() logic that checks for required properties
    This no longer necessary with attributes which can define required and optional properties

--- modules/core/data_stream/src/DataStreamTypeManager.php
+++ modules/core/data_stream/src/DataStreamTypeManager.php
@@ -39,17 +39,4 @@ class DataStreamTypeManager extends DefaultPluginManager {
     $this->setCacheBackend($cache_backend, 'data_stream_type_plugins');
   }
 
-  /**
-   * {@inheritdoc}
-   */
-  public function processDefinition(&$definition, $plugin_id) {
-    parent::processDefinition($definition, $plugin_id);
-
-    foreach (['id', 'label'] as $required_property) {
-      if (empty($definition[$required_property])) {
-        throw new PluginException(sprintf('The data stream type %s must define the %s property.', $plugin_id, $required_property));
-      }
-    }
-  }
-
 }
--- modules/core/entity/src/LogTypeManager.php
+++ modules/core/entity/src/LogTypeManager.php
@@ -39,17 +39,4 @@ class LogTypeManager extends DefaultPluginManager {
     $this->setCacheBackend($cache_backend, 'log_type_plugins');
   }
 
-  /**
-   * {@inheritdoc}
-   */
-  public function processDefinition(&$definition, $plugin_id) {
-    parent::processDefinition($definition, $plugin_id);
-
-    foreach (['id', 'label'] as $required_property) {
-      if (empty($definition[$required_property])) {
-        throw new PluginException(sprintf('The log type %s must define the %s property.', $plugin_id, $required_property));
-      }
-    }
-  }
-
 }
