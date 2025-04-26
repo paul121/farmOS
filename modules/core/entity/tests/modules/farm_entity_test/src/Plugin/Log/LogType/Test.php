24d1b2bd19c693451c9629484ed01332717f91d2
Paul Weidner <paul.weidner@gmail.com>
2025-04-25 19:50:52 -0700

    Update test plugin attributes

--- modules/core/entity/tests/modules/farm_entity_test/src/Plugin/Log/LogType/Test.php
+++ modules/core/entity/tests/modules/farm_entity_test/src/Plugin/Log/LogType/Test.php
@@ -4,16 +4,17 @@ declare(strict_types=1);
 
 namespace Drupal\farm_entity_test\Plugin\Log\LogType;
 
+use Drupal\Core\StringTranslation\TranslatableMarkup;
+use Drupal\farm_entity\Attribute\LogType;
 use Drupal\farm_entity\Plugin\Log\LogType\FarmLogType;
 
 /**
  * Provides the test log type.
- *
- * @LogType(
- *   id = "test",
- *   label = @Translation("Test"),
- * )
  */
+#[LogType(
+  id: 'test',
+  label: new TranslatableMarkup('Test'),
+)]
 class Test extends FarmLogType {
 
   /**
